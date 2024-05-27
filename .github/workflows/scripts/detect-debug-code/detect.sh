4ff47fc791777eb3941f155f58bb1352e2265580
KogaTakumi <koga@rockwave.co.jp>
2024-05-27 16:19:04 +0900

    chore: Update debug code detection workflow to include JavaScript files

--- .github/workflows/scripts/detect-debug-code/detect.sh
+++ .github/workflows/scripts/detect-debug-code/detect.sh
@@ -1,23 +1,23 @@
-# files=$@
-files=git diff origin/main --name-only
+files=$@
 debug_code_exists=false
 
 for file in ${files[@]}; do
-    # switchで分岐
     case ${file##*.} in
         "js")
-            debug_code=(`grep -n -E 'console.log\(|' ${file}`)
+            IFS=$'\n' read -r -d '' -a debug_codes < <(grep -n -E 'console.log\(' ${file})
             ;;
         "php")
-            debug_code=(`grep -n -E 'debug\(|var_dump\(|dump\(' ${file}`)
+            IFS=$'\n' read -r -d '' -a debug_codes < <(grep -n -E 'debug\(|var_dump\(|dump\(' ${file})
             ;;
         *)
-            debug_code=()
+            debug_codes=()
             ;;
     esac
 
-    if [ -n "$debug_code" ]; then
-        echo "[detected] $file: $debug_code"
+    if [ -n "$debug_codes" ]; then
         debug_code_exists=true
+        for debug_code in "${debug_codes[@]}"; do
+            echo "[detected] $file:$debug_code"
+        done
     fi
 done
