5f60c1a00f67ade64bb41f5b0bd28c1f838c1b4b
KogaTakumi <koga@rockwave.co.jp>
2024-05-27 14:55:04 +0900

    chore: Update debug code detection workflow to include JavaScript files

---
+++ .github/workflows/scripts/detect-debug-code/detect-js.sh
@@ -0,0 +1,20 @@
+files=$1
+debug_code_exists=false
+
+for file in ${files[@]}; do
+    if [ ${file: -3} != ".js" ]; then
+        continue
+    fi
+
+    echo "Checking $file"
+    debug_code=(`grep -E 'console.log\(|' ${file}`)
+    if [ -n "$debug_code" ]; then
+        echo "$file: $debug_code"
+        debug_code_exists=true
+    fi
+done
+
+if $debug_code_exists; then
+    echo "Debug code exists in the diff. Please remove it."
+    exit 1
+fi
