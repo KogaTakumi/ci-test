f0715517f8255335551d6c127e24219d25309c92
KogaTakumi <koga@rockwave.co.jp>
2024-06-04 10:40:20 +0900

    Refactor debug code detection script to use git diff for file list

--- .github/workflows/scripts/detect-debug-code/detect.sh
+++ .github/workflows/scripts/detect-debug-code/detect.sh
@@ -2,28 +2,30 @@ files=$@
 debug_code_exists=false
 
 for file in ${files[@]}; do
-    case ${file##*.} in
-        "js")
-            IFS=$'\n' read -r -d '' -a debug_codes < <(grep -n -E 'console.log\(' ${file})
-            ;;
-        "php")
-            IFS=$'\n' read -r -d '' -a debug_codes < <(grep -n -E 'debug\(|var_dump\(|dump\(' ${file})
-            ;;
-        *)
-            debug_codes=()
-            ;;
-    esac
+    if [ -e file ]; then
+        case ${file##*.} in
+            "js")
+                IFS=$'\n' read -r -d '' -a debug_codes < <(grep -n -E 'console.log\(' ${file})
+                ;;
+            "php")
+                IFS=$'\n' read -r -d '' -a debug_codes < <(grep -n -E 'debug\(|var_dump\(|dump\(' ${file})
+                ;;
+            *)
+                debug_codes=()
+                ;;
+        esac
 
-    if [ -n "$debug_codes" ]; then
-        debug_code_exists=true
-        for debug_code in "${debug_codes[@]}"; do
-            IFS=':' read -r -a debug_code_parts <<< "$debug_code"
-            echo "::error:: Debug code found in ${file}:${debug_code_parts[0]}: \`${debug_code_parts[1]}\`"
-            # For reviewdog
-            # In github actions, reviewdog is used to comment on PRs
-            # reviewdog -efm="%f:%l:%c: %m" -name="detect-debug-code" -reporter=github-pr-review -level=error
-            # echo "${file}:${debug_code_parts[0]}:0: Debug code found \`${debug_code_parts[1]}\`"
-        done
+        if [ -n "$debug_codes" ]; then
+            debug_code_exists=true
+            for debug_code in "${debug_codes[@]}"; do
+                IFS=':' read -r -a debug_code_parts <<< "$debug_code"
+                echo "::error:: Debug code found in ${file}:${debug_code_parts[0]}: \`${debug_code_parts[1]}\`"
+                # For reviewdog
+                # In github actions, reviewdog is used to comment on PRs
+                # reviewdog -efm="%f:%l:%c: %m" -name="detect-debug-code" -reporter=github-pr-review -level=error
+                # echo "${file}:${debug_code_parts[0]}:0: Debug code found \`${debug_code_parts[1]}\`"
+            done
+        fi
     fi
 done
 
